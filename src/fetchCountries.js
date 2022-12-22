c9b5150d6182cf5269beb937204fac5961800e6c
OlegRosinskyi <olegrosinskyi@gmail.com>
2022-12-22 14:07:28 +0200

    update fetchCountries.js

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,3 +1,3 @@
 export default function fetchCountries(country) {
-  return fetch(`https:restcountries.com/v3.1/name/${country}`);
+  return fetch(`https:restcountries.com/v3.1/name/${country}/`);
 }
