1f5f09b835dae649a496c561690bef85500a643e
OlegRosinskyi <olegrosinskyi@gmail.com>
2023-01-04 06:41:56 +0200

    update index.js

--- src/index.js
+++ src/index.js
@@ -7,14 +7,15 @@ import fetchCountries from './fetchCountries';
 const inputEl = document.querySelector('#search-box');
 //const divEl = document.querySelector('.country-info');
 const ulEl = document.querySelector('.country-list');
+const divEl = document.querySelector('.country-info');
 
 const articleElement = articls => {
   return articls
     .map(({ name, capital, population, flags, languages, cca3 }, index) => {
       if (articls.length === 1) {
-        return `<li> <div class="country-info">
+        return `
    
-   <ul class="list">
+   <ul class="country-list list">
     <li> <ul class="all_list list">
     <li class="list__item"><img src="${flags.svg}" width="20px"alt="flag"> </li>
     <li class="list__item">  <h1  class="item_text_h" >${
@@ -35,17 +36,16 @@ const articleElement = articls => {
     </ul> </li>
 
     </ul>
-    </div>
-    </li>`;
+   `;
       } else {
-        return `<li> <div class="country-info">
-   
+        return `
+   <li class="list__item">
    <ul class="all_list list">
     <li class="list__item"><img src="${flags.svg}" width="20px"alt="flag"> </li>
     <li class="list__item">  <p  class="item_text" >${name.common}</p> </li> 
     </ul>
-    </div>
-    </li>`;
+    </li>
+    `;
       }
     })
     .join('');
@@ -55,31 +55,50 @@ console.log(inputEl);
 const DEBOUNCE_DELAY = 300;
 
 const onInput = event => {
-  fetchCountries(event.target.value.trim())
-    .then(Response => Response.json())
-    .then(articls => {
-      console.log(articls);
-      if (articls.status === 404) {
-        ulEl.innerHTML = '';
-        Notiflix.Notify.failure('Oops, there is no country with that name');
-      } else {
-        console.log(articls[0].languages[0]);
-        //const lang = articls[0].cca3.toLowerCase();
-        if (articls.length > 10) {
+  console.log(event.target.value.length);
+  const valuelongth = event.target.value.length;
+  const valuesString = event.target.value;
+  let element = '';
+  for (let index = 0; index < valuelongth; index++) {
+    element = element + ' ';
+    //console.log(element);
+  }
+  //console.log(event.target.value === element);
+  if (event.target.value === element) return;
+  else {
+    fetchCountries(event.target.value.trim())
+      .then(Response => Response.json())
+      .then(articls => {
+        console.log(articls);
+        if (articls.status === 404) {
           ulEl.innerHTML = '';
-          Notiflix.Notify.info(
-            'Too many matches found. Please enter a more specific name.'
-          );
+          Notiflix.Notify.failure('Oops, there is no country with that name');
         } else {
-          console.log(articls);
-          ulEl.innerHTML = articleElement(articls);
+          console.log(articls[0].languages[0]);
+          //const lang = articls[0].cca3.toLowerCase();
+          if (articls.length > 10) {
+            ulEl.innerHTML = '';
+            divEl.innerHTML = '';
+            Notiflix.Notify.info(
+              'Too many matches found. Please enter a more specific name.'
+            );
+          } else {
+            if (articls.length > 1) {
+              divEl.innerHTML = '';
+              ulEl.innerHTML = articleElement(articls);
+            } else {
+              ulEl.innerHTML = '';
+              divEl.innerHTML = articleElement(articls);
+            }
+          }
         }
-      }
-    })
-    .catch(err => {
-      console.log(err);
-      ulEl.innerHTML = '';
-    });
+      })
+      .catch(err => {
+        console.log(err);
+        ulEl.innerHTML = '';
+        divEl.innerHTML = '';
+      });
+  }
 };
 
 inputEl.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
